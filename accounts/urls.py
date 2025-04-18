f2889227e23d15eff468d624a242f8a7cba24c62
Vibhorgaur2005 <Vibhorgaur2005@gmail.com>
2025-04-19 00:57:17 +0530

    Hi

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.contrib import admin
+from django.urls import path, include
 urlpatterns = [
     path('', views.home, name='home'),
     path("signup/",views.signup_view, name="signup"),
@@ -11,7 +12,7 @@ urlpatterns = [
     path('sustainability-score/', views.sustainability_score, name='sustainability_score'),
     path('profile/', views.profile, name='profile'),
     path('community-insights/', views.community_insights, name='community_insights'),
-     path('logout/', views.logout_page, name='logout'),
+    path('logout/', views.logout_page, name='logout'),
     path('communities/', views.community_insights, name='community_insights'),
     path('sustainability_calculator/', views.sustainability_calculator, name='sustainability_calculator'),
     path('community-insights/', views.community_insights_all, name='community_insights_all'),
