66ac5ffa0c0ff5cd9fc72d17e7187f2d62bfabba
Vibhorgaur2005 <Vibhorgaur2005@gmail.com>
2025-04-23 01:47:51 +0530

    Add Render domain to ALLOWED_HOSTS

--- ecostat/settings.py
+++ ecostat/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = 'django-insecure-dt_*3=2-gtzr+x_235rao+dy$le3g5dxa39_r%45p2l&ztgip$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dti-ecostat-3.onrender.com']
-
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dti-ecostat-3.onrender.com']
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "dti-ecostat-7.onrender.com",  # ✅ Add this
+]
 
 # Application definition
 
